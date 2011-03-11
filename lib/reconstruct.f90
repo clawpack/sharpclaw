69c4b7bab811355d4e6ee800a1e2ef46aa1c0beb
David Ketcheson <dketch@gmail.com>
2011-03-11 14:21:26 +0300

    Add f-wave-slope reconstruction.

--- lib/reconstruct.f90
+++ lib/reconstruct.f90
@@ -427,6 +427,99 @@ contains
 
     end subroutine weno5_wave
 
+    ! ===================================================================
+    subroutine weno5_fwave(q,ql,qr,fwave,s)
+    ! ===================================================================
+    !
+    !  Fifth order WENO reconstruction, based on f-waves
+    !  that are interpreted as slopes.
+!
+
+      implicit double precision (a-h,o-z)
+
+      double precision, intent(in) :: q(:,:)
+      double precision, intent(inout) :: fwave(:,:,:), s(:,:)
+      double precision, intent(out) :: ql(:,:), qr(:,:)
+      double precision  u(20,2)
+
+      integer, parameter :: mbc=3
+      integer :: meqn, mx2
+
+      mx2= size(q,1); meqn = size(q,2); mwaves=size(fwave,3)
+
+      ! convert fwaves to waves by dividing by the sound speed
+      ! We do this in place to save memory
+      ! and get away with it because the waves are never used again
+      forall(i=1:mx2,mw=1:mwaves,m=1:meqn)
+          fwave(i,m,mw)=fwave(i,m,mw)/s(i,mw)
+      end forall
+
+      ! loop over interfaces (i-1/2)
+      do i=2,mx2
+        ! Compute the part of the reconstruction that is
+        !  stencil-independent
+        do m=1,meqn
+          qr(i-1,m) = q(i-1,m)
+          ql(i  ,m) = q(i,m)
+        enddo
+        ! the reconstruction is performed in terms of waves
+        do mw=1,mwaves
+         ! loop over which side of x_i-1/2 we're on
+          do m1=1,2
+            ! m1=1: construct q^-_{i-1/2}
+            ! m1=2: construct q^+_{i-1/2}
+
+            im=(-1)**(m1+1)
+            ione=im; inone=-im; intwo=-2*im
+  
+            ! compute projections of waves in each family
+            ! onto the corresponding wave at the current interface
+            wnorm2 = fwave(i      ,1,mw)*fwave(i,1,mw)
+            theta1 = fwave(i+intwo,1,mw)*fwave(i,1,mw)
+            theta2 = fwave(i+inone,1,mw)*fwave(i,1,mw)
+            theta3 = fwave(i+ione ,1,mw)*fwave(i,1,mw)
+            do m=2,meqn
+              wnorm2 = wnorm2 + fwave(i,      m,mw)*fwave(i,m,mw)
+              theta1 = theta1 + fwave(i+intwo,m,mw)*fwave(i,m,mw)
+              theta2 = theta2 + fwave(i+inone,m,mw)*fwave(i,m,mw)
+              theta3 = theta3 + fwave(i+ione ,m,mw)*fwave(i,m,mw)
+            enddo
+!
+             t1=im*(theta1-theta2)
+             t2=im*(theta2-wnorm2)
+             t3=im*(wnorm2-theta3)
+  
+             tt1=13.*t1**2+3.*(theta1   -3.*theta2)**2
+             tt2=13.*t2**2+3.*(theta2   +   wnorm2)**2
+             tt3=13.*t3**2+3.*(3.*wnorm2-   theta3)**2
+       
+             tt1=(epweno+tt1)**2
+             tt2=(epweno+tt2)**2
+             tt3=(epweno+tt3)**2
+             s1 =tt2*tt3
+             s2 =6.*tt1*tt3
+             s3 =3.*tt1*tt2
+             t0 =1./(s1+s2+s3)
+             s1 =s1*t0
+             s3 =s3*t0
+  
+           if(wnorm2.gt.1.e-14) then
+             u(mw,m1) = ( (s1*(t2-t1)+(0.5*s3-0.25)*(t3-t2))/3. &
+                       + im*(theta2+6.d0*wnorm2-theta3)/12.d0)
+             wnorm2=1.d0/wnorm2
+           else
+             u(mw,m1) = 0.d0
+             wnorm2=0.d0
+           endif
+          enddo !end loop over which side of interface
+          do m=1,meqn
+            qr(i-1,m) = qr(i-1,m) +  u(mw,1)*fwave(i,m,mw)*wnorm2
+            ql(i  ,m) = ql(i  ,m) +  u(mw,2)*fwave(i,m,mw)*wnorm2
+          enddo
+        enddo !loop over fwaves
+      enddo !loop over interfaces
+
+    end subroutine weno5_fwave
 
     ! ===================================================================
     subroutine tvd2(q,ql,qr,mthlim)
